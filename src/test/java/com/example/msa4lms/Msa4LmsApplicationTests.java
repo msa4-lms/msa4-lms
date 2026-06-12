71bf7459a8c74e9972e77f82144aedf31e48a130
jinny-do <sonmojiri7@gmail.com>
2026-06-12 13:15:00 +0900

    260612

---
+++ src/test/java/com/example/msa4lms/Msa4LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msa4lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Msa4LmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
