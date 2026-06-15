087f22988121d765f235bf71190743ebbbb859e9
HongD93 <tigerson93@gmail.com>
2026-06-15 15:09:53 +0900

    [260615] fix: 강의 검색 페이징 오류 수정

--- src/main/java/com/msa4lms/domain/lecture/controllers/LectureController.java
+++ src/main/java/com/msa4lms/domain/lecture/controllers/LectureController.java
@@ -2,7 +2,6 @@ package com.msa4lms.domain.lecture.controllers;
 
 import com.msa4lms.domain.lecture.requests.LectureSearchReq;
 import com.msa4lms.domain.lecture.responses.LecturePagedRes;
-import com.msa4lms.domain.lecture.responses.LectureRes;
 import com.msa4lms.domain.lecture.services.LectureService;
 import com.msa4lms.global.responses.GlobalRes;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
