206270d1b22aee2579078164bc12f4a78c181f5b
jinny-do <sonmojiri7@gmail.com>
2026-06-25 16:46:17 +0900

    폴더 경로 수정

--- src/main/java/com/msa4lms/domain/grade/mapper/GradeMapper.java
+++ src/main/java/com/msa4lms/domain/grade/mapper/GradeMapper.java
@@ -1,13 +1,13 @@
 package com.msa4lms.domain.grade.mapper;
 
-import com.msa4lms.domain.grade.entities.Grade;
+import com.msa4lms.domain.grade.responses.SemesterGradeRes;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 @Mapper
 public interface GradeMapper {
-    List<com.msa4lms.domain.grade.responses.SemesterGradeRes> findGradesByStudentId(
+    List<SemesterGradeRes> findGradesByStudentId(
             @Param("studentId") long studentId,
             @Param("year") Integer year,
             @Param("semester") Integer semester);
