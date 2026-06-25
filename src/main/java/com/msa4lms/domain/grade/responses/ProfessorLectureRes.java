b53d252aa1d9b7641dbcba70aa9626276f9b5c6d
jinny-do <sonmojiri7@gmail.com>
2026-06-25 10:34:38 +0900

    성적 정정 화면

---
+++ src/main/java/com/msa4lms/domain/grade/responses/ProfessorLectureRes.java
@@ -0,0 +1,18 @@
+package com.msa4lms.domain.grade.responses;
+
+import lombok.Builder;
+
+@Builder
+public record ProfessorLectureRes(
+        Long lectureId,
+        String courseCode,
+        String departmentName,
+        String courseName,
+        Integer credit,
+        Integer targetGrade,
+        String professorName,
+        String classroom,
+        String schedule,
+        Integer capacity
+) {
+}
